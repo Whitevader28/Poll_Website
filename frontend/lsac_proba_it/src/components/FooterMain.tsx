1350e1119e6a1825492646661d178835b4727484
Whitevader28 <alexutu2020@yahoo.com>
2023-11-25 23:03:12 +0200

    added vw on margin just for the sake of it, still couldnt center the div :((

--- frontend/lsac_proba_it/src/components/FooterMain.tsx
+++ frontend/lsac_proba_it/src/components/FooterMain.tsx
@@ -5,15 +5,29 @@ function FooterMain() {
   return (
     <>
       <footer id="my-footer">
-        <a className="link-btn" href="https://www.instagram.com/lsacbucuresti/">
-          <img src="src\assets\mdi_instagram.svg" alt="Follow on insta" />
-        </a>
-        <a className="link-btn" href="https://www.facebook.com/LsacBucuresti">
-          <img src="src\assets\uil_facebook.svg" alt="Follow on insta" />
-        </a>
-        <a className="link-btn" href="https://www.twitch.tv/lsac_bucuresti">
-          <img src="src\assets\mdi_twitch.svg" alt="Follow on insta" />
-        </a>
+        <div className="link-btn-container">
+          <a href="https://www.instagram.com/lsacbucuresti/">
+            <img
+              className="link-btn"
+              src="src\assets\mdi_instagram.svg"
+              alt="Follow on insta"
+            />
+          </a>
+          <a href="https://www.facebook.com/LsacBucuresti">
+            <img
+              className="link-btn"
+              src="src\assets\uil_facebook.svg"
+              alt="Follow on insta"
+            />
+          </a>
+          <a href="https://www.twitch.tv/lsac_bucuresti">
+            <img
+              className="link-btn"
+              src="src\assets\mdi_twitch.svg"
+              alt="Follow on insta"
+            />
+          </a>
+        </div>
       </footer>
     </>
   );
