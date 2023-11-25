7800ee9c24a9c26bf7faafa835ac823472c65dee
Whitevader28 <alexutu2020@yahoo.com>
2023-11-25 21:23:08 +0200

    added links and images to footer social buttons

--- frontend/lsac_proba_it/src/components/FooterMain.tsx
+++ frontend/lsac_proba_it/src/components/FooterMain.tsx
@@ -4,8 +4,17 @@ import "./cmp_styles/footer.css";
 function FooterMain() {
   return (
     <>
-      {/* TODO: Add image buttons and make them flex like the navbar was at the beginning */}
-      <footer id="my-footer">SOME</footer>
+      <footer id="my-footer">
+        <a className="link-btn" href="https://www.instagram.com/lsacbucuresti/">
+          <img src="src\assets\mdi_instagram.svg" alt="Follow on insta" />
+        </a>
+        <a className="link-btn" href="https://www.facebook.com/LsacBucuresti">
+          <img src="src\assets\uil_facebook.svg" alt="Follow on insta" />
+        </a>
+        <a className="link-btn" href="https://www.twitch.tv/lsac_bucuresti">
+          <img src="src\assets\mdi_twitch.svg" alt="Follow on insta" />
+        </a>
+      </footer>
     </>
   );
 }
