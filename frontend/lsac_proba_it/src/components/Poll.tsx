052f6a6745ff037fb15566a9b5f25e3ae55c60eb
Whitevader28 <alexutu2020@yahoo.com>
2023-12-05 17:00:40 +0200

    made the poll grid look good and colored button

--- frontend/lsac_proba_it/src/components/Poll.tsx
+++ frontend/lsac_proba_it/src/components/Poll.tsx
@@ -1,14 +1,15 @@
-import "./cmp_styles/Poll.css";
+import "./cmp_styles/poll.css";
 
 function Poll() {
   function handleVote(e: any) {
     e.preventDefault();
     const formData = new FormData(e.target);
-    const selectedOption = formData.get(pollID);
+    const selectedOption = formData.get(pollID1); // this isn't working just temp fix
     console.log(selectedOption);
   }
 
-  const pollID = "1";
+  const pollID1 = "1";
+  // const pollID2 = "2";
   const option1 = "Option 1";
   const option2 = "Option 2";
   const question = "What is your favourite option?";
@@ -19,16 +20,52 @@ function Poll() {
         <div className="individual-poll">
           <div className="question-text">{question}</div>
           <p className="make-a-choice">Make a choice:</p>
-          <form onSubmit={handleVote}>
+          <form onSubmit={handleVote} className="radio-form">
             <label>
-              <input name={pollID} type="radio" value={option1} />
+              <input name={pollID1} type="radio" value={option1} />
               {option1}
             </label>
             <label>
-              <input name={pollID} type="radio" value={option2} />
+              <input name={pollID1} type="radio" value={option2} />
               {option2}
             </label>
-            <button type="submit">Vote</button>
+            <button className="radio-form-btn" type="submit">
+              Vote
+            </button>
+          </form>
+        </div>
+        <div className="individual-poll">
+          <div className="question-text">{question}</div>
+          <p className="make-a-choice">Make a choice:</p>
+          <form onSubmit={handleVote} className="radio-form">
+            <label>
+              <input name={pollID1} type="radio" value={option1} />
+              {option1}
+            </label>
+            <label>
+              <input name={pollID1} type="radio" value={option2} />
+              {option2}
+            </label>
+            <button className="radio-form-btn" type="submit">
+              Vote
+            </button>
+          </form>
+        </div>{" "}
+        <div className="individual-poll">
+          <div className="question-text">{question}</div>
+          <p className="make-a-choice">Make a choice:</p>
+          <form onSubmit={handleVote} className="radio-form">
+            <label>
+              <input name={pollID1} type="radio" value={option1} />
+              {option1}
+            </label>
+            <label>
+              <input name={pollID1} type="radio" value={option2} />
+              {option2}
+            </label>
+            <button className="radio-form-btn" type="submit">
+              Vote
+            </button>
           </form>
         </div>
       </div>
